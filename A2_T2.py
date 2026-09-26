03d56b4bb82d17c9557036a4f986bfac65617d50
vaniasadeghi1990-art <vania.sadeghi1990@gmail.com>
2026-09-26 13:02:10 +0300

    Delete A2_T2.py

--- A2_T2.py
+++
@@ -1,6 +0,0 @@
-print("Program starting.")
-brand= input ("Insert car brand: ")
-model= input ("Insert car model: ")
-print(f'Car brand is \"{brand}\" and the model is \'{model}\'.')
-print ("Program ending.")
-
