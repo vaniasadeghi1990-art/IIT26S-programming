52c2cc79091908ebaf83b629c6f74d75973735b9
vaniasadeghi1990-art <vania.sadeghi1990@gmail.com>
2026-09-26 13:00:56 +0300

    Delete A2_T7.py

--- A2_T7.py
+++
@@ -1,6 +0,0 @@
-print("Program starting.")
-fahrenheit = float(input("Insert fahrenheits: "))
-celsius = (fahrenheit - 32) / 1.8
-celsius_rounded = round(celsius, 1)
-print(f"{fahrenheit}°F is {celsius_rounded}°C")
-print("Program ending.")
